e560f32293d18336c35fe551bf6e2aefae913daa
Johnny Oshika <johnnyoshika@gmail.com>
2016-01-27 09:20:38 -0800

    Add config.

---
+++ Demo/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.OptionsModel;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Demo
+{
+    public class HomeController : Controller
+    {
+        public HomeController(IOptions<Config> config)
+        {
+            Config = config.Value;
+        }
+
+        Config Config { get; }
+
+        public IActionResult Index()
+        {
+            return Content($"Name: {Config.Name}\nAge: {Config.Age}");
+        }
+    }
+}
